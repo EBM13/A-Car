537bd9ee43fe455d8252af79e062126f1fd5025a
EBM13 <enricoxbmx@gmail.com>
2023-05-01 01:04:50 -0500

    coma arreglada

--- comandos.js
+++ comandos.js
@@ -6,11 +6,12 @@ window.onmousedown = e => {
 
 window.onmouseup = () => {
     track.dataset.mouseDownAt = "0";
-}
+} 
+
 
 window.onmousemove = e => {
     if(track.dataset.mouseDownAt === "0") return;
-    const mouseDelta = parseFloat(track,dataset.mouseDownAt) - e.clientX,
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
         maxDelta = window.innerWidth / 2;
 
     const percentage = (mouseDelta/maxDelta)* -100;
